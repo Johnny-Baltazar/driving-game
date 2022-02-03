c811a55466a3192a3f7ab2dad6ac84730a384b5d
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-02-03 15:06:22 -0800

    Create a function to stop car. #4

--- driving-game/main.js
+++ driving-game/main.js
@@ -1,4 +1,12 @@
 var $raceCar = document.querySelector('.race-car');
+var raceCarData = {
+  position: 'east',
+  location: {
+    x: 0,
+    y: 0
+  },
+  started: false
+};
 
 document.addEventListener('keydown', turnCar);
 
@@ -14,5 +22,21 @@ function turnCar(event) {
   } else if (event.code === 'ArrowLeft') {
     $raceCar.classList.add('left');
     $raceCar.classList.remove('up', 'down');
+  }
+}
+
+document.addEventListener('keydown', moveCar);
+
+var moveCarInterval;
+
+function moveCar(event) {
+  if (event.code === 'Space' && raceCarData.started === false) {
+    raceCarData.started = true;
+    moveCarInterval = setInterval(function () {
+      raceCarData.location.x = raceCarData.location.x + 16;
+      $raceCar.style.left = raceCarData.location.x + 'px';
+    }, 16);
+  } else if (event.code === 'Space' && raceCarData.started === true) {
+    clearInterval(moveCarInterval);
   }
 }
