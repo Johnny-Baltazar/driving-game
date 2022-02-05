036fc20c526596e5ed655fb114173fc8901b69fa
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-02-05 14:07:40 -0800

    Issue-5-user-can-steer-their-car. #5

--- driving-game/main.js
+++ driving-game/main.js
@@ -1,4 +1,5 @@
 var $raceCar = document.querySelector('.race-car');
+var $explosion = document.querySelector('.explosion');
 var raceCarData = {
   position: 'east',
   location: {
@@ -37,10 +38,47 @@ function moveCar(event) {
   if (event.code === 'Space' && raceCarData.started === false) {
     raceCarData.started = true;
     moveCarInterval = setInterval(function () {
-      raceCarData.location.x = raceCarData.location.x + 16;
-      $raceCar.style.left = raceCarData.location.x + 'px';
+      if (raceCarData.position === 'east') {
+        raceCarData.location.x = raceCarData.location.x + 16;
+        $raceCar.style.left = raceCarData.location.x + 'px';
+        if (raceCarData.location.x >= 1300) {
+          clearInterval(moveCarInterval);
+          $explosion.classList.remove('hidden');
+          $explosion.style.left = 1350 + 'px';
+          $explosion.style.top = raceCarData.location.y - 60 + 'px';
+        }
+      } else if (raceCarData.position === 'south') {
+        raceCarData.location.y = raceCarData.location.y + 16;
+        $raceCar.style.top = raceCarData.location.y + 'px';
+        if (raceCarData.location.y >= 600) {
+          clearInterval(moveCarInterval);
+          $explosion.classList.remove('hidden');
+          $explosion.style.left = raceCarData.location.x + 20 + 'px';
+          $explosion.style.top = raceCarData.location.y - 60 + 'px';
+        }
+        // $explosion.classList.remove('hidden');
+      } else if (raceCarData.position === 'west') {
+        raceCarData.location.x = raceCarData.location.x - 16;
+        $raceCar.style.left = raceCarData.location.x + 'px';
+        if (raceCarData.location.x === -128) {
+          clearInterval(moveCarInterval);
+          $explosion.classList.remove('hidden');
+          $explosion.style.left = raceCarData.location.x + 20 + 'px';
+          $explosion.style.top = raceCarData.location.y - 60 + 'px';
+        }
+      } else if (raceCarData.position === 'north') {
+        raceCarData.location.y = raceCarData.location.y - 16;
+        $raceCar.style.top = raceCarData.location.y + 'px';
+        if (raceCarData.location.y === -16) {
+          clearInterval(moveCarInterval);
+          $explosion.classList.remove('hidden');
+          $explosion.style.left = raceCarData.location.x + 20 + 'px';
+          $explosion.style.top = raceCarData.location.y - 60 + 'px';
+        }
+      }
     }, 16);
   } else if (raceCarData.started === true && event.code === 'Space') {
+    raceCarData.started = false;
     clearInterval(moveCarInterval);
   }
 }
